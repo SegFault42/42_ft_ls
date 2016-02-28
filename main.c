51e186965cf8469f59e2e9be776262216b9d627b
Ramzi BOUGUERRA <rabougue@e1r11p23.42.fr>
2016-02-28 17:20:37 +0100

    ft_ls

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rabougue <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/28 11:34:55 by rabougue          #+#    #+#             */
+/*   Updated: 2016/02/28 12:23:44 by rabougue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./ft_ls.h"
 
 int main(int argc, const char *argv[])
 {
-	DIR *rep = NULL;
-	struct dirent *fichierlu = NULL;
+	DIR				*rep = NULL;
+	struct dirent	*fichierlu = NULL;
+	struct stat		info;
+	char			file[5];
 	int i = 0;
 
 	if (argc == 1 && (rep = opendir("./")) != NULL)
 	{
 		while((fichierlu = readdir(rep)) != NULL)
-			ft_putendl(fichierlu->d_name);
+		{
+			file[i] = fichierlu->d_name;
+			i++;
+		}
 	}
+	stat(argv[1], &info);
+	ft_putnbr(info.st_blocks);
 	/*if (argc == 2 && (rep = opendir(argv[1])) == NULL)*/
 	/*{*/
 		/*perror("Error ");*/
